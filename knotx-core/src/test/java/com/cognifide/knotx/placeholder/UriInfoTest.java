f037622ac80542f688c7e14523f10b33d36205ab
Dominik Przybyl <przybyl.dominik@gmail.com>
2016-08-16 01:28:52 +0200

    Placeholders in service-url for dynamic parameters (#8)
    added zohhak

--- knotx-core/src/test/java/com/cognifide/knotx/placeholder/UriInfoTest.java
+++ knotx-core/src/test/java/com/cognifide/knotx/placeholder/UriInfoTest.java
@@ -14,68 +14,47 @@
  */
 package com.cognifide.knotx.placeholder;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(Parameterized.class)
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+
+@RunWith(ZohhakRunner.class)
 public class UriInfoTest {
 
-    @Parameterized.Parameters(name = "{index}: {0}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                { "/a/b", "/a/b", null, null, null },
-                { "/a/b.html", "/a/b", null, "html", null },
-                { "/a/b.s1.html", "/a/b", "s1", "html", null },
-                { "/a/b.s1.s2.html", "/a/b", "s1.s2", "html", null },
-                { "/a/b.s1.html/c/d", "/a/b", "s1", "html", "/c/d" },
-                { "/a/b.s1.s2.html/c/d", "/a/b", "s1.s2", "html", "/c/d" },
-                { "/a/b.html/c/d.s.txt", "/a/b", null, "html", "/c/d.s.txt" },
-                { "/a/b.s1.html/c/d.s.txt", "/a/b", "s1", "html", "/c/d.s.txt" },
-                { "/a/b.s1.s2.html/c/d.s.txt", "/a/b", "s1.s2", "html", "/c/d.s.txt" },
-                { "/a/b?q=v", "/a/b", null, null, null },
-                { "/a/b.html?q=v", "/a/b", null, "html", null },
-                { "/a/b.s1.html?q=v", "/a/b", "s1", "html", null },
-                { "/a/b.s1.s2.html?q=v", "/a/b", "s1.s2", "html", null },
-                { "/a/b.s1.html/c/d?q=v", "/a/b", "s1", "html", "/c/d" },
-                { "/a/b.s1.s2.html/c/d?q=v", "/a/b", "s1.s2", "html", "/c/d" },
-                { "/a/b.html/c/d.s.txt?q=v", "/a/b", null, "html", "/c/d.s.txt" },
-                { "/a/b.s1.html/c/d.s.txt?q=v", "/a/b", "s1", "html", "/c/d.s.txt" },
-                { "/a/b.s1.s2.html/c/d.s.txt?q=v", "/a/b", "s1.s2", "html", "/c/d.s.txt" },
-                { "/a/b#f", "/a/b", null, null, null },
-                { "/a/b.html#f", "/a/b", null, "html", null },
-                { "/a/b.s1.html#f", "/a/b", "s1", "html", null },
-                { "/a/b.s1.s2.html#f", "/a/b", "s1.s2", "html", null },
-                { "/a/b.s1.html/c/d#f", "/a/b", "s1", "html", "/c/d" },
-                { "/a/b.s1.s2.html/c/d#f", "/a/b", "s1.s2", "html", "/c/d" },
-                { "/a/b.html/c/d.s.txt#f", "/a/b", null, "html", "/c/d.s.txt" },
-                { "/a/b.s1.html/c/d.s.txt#f", "/a/b", "s1", "html", "/c/d.s.txt" },
-                { "/a/b.s1.s2.html/c/d.s.txt#f", "/a/b", "s1.s2", "html", "/c/d.s.txt" }
-        });
-    }
-
-    @Parameterized.Parameter
-    public String uri;
-
-    @Parameterized.Parameter(value = 1)
-    public String path;
-
-    @Parameterized.Parameter(value = 2)
-    public String selectorString;
-
-    @Parameterized.Parameter(value = 3)
-    public String extension;
-
-    @Parameterized.Parameter(value = 4)
-    public String suffix;
-
-    @Test
-    public void test() {
+    @TestWith({
+            "/a/b,                          /a/b, null,  null, null",
+            "/a/b.html,                     /a/b, null,  html, null",
+            "/a/b.s1.html,                  /a/b, s1,    html, null",
+            " /a/b.s1.s2.html,              /a/b, s1.s2, html, null",
+            "/a/b.s1.html/c/d,              /a/b, s1,    html, /c/d",
+            "/a/b.s1.s2.html/c/d,           /a/b, s1.s2, html, /c/d",
+            "/a/b.html/c/d.s.txt,           /a/b, null,  html, /c/d.s.txt",
+            "/a/b.s1.html/c/d.s.txt,        /a/b, s1,    html, /c/d.s.txt",
+            "/a/b.s1.s2.html/c/d.s.txt,     /a/b, s1.s2, html, /c/d.s.txt",
+            "/a/b?q=v,                      /a/b, null,  null, null",
+            "/a/b.html?q=v,                 /a/b, null,  html, null",
+            "/a/b.s1.html?q=v,              /a/b, s1,    html, null",
+            "/a/b.s1.s2.html?q=v,           /a/b, s1.s2, html, null",
+            "/a/b.s1.html/c/d?q=v,          /a/b, s1,    html, /c/d",
+            "/a/b.s1.s2.html/c/d?q=v,       /a/b, s1.s2, html, /c/d",
+            "/a/b.html/c/d.s.txt?q=v,       /a/b, null,  html, /c/d.s.txt",
+            "/a/b.s1.html/c/d.s.txt?q=v,    /a/b, s1,    html, /c/d.s.txt",
+            "/a/b.s1.s2.html/c/d.s.txt?q=v, /a/b, s1.s2, html, /c/d.s.txt",
+            "/a/b#f,                        /a/b, null,  null, null",
+            "/a/b.html#f,                   /a/b, null,  html, null",
+            "/a/b.s1.html#f,                /a/b, s1,    html, null",
+            "/a/b.s1.s2.html#f,             /a/b, s1.s2, html, null",
+            "/a/b.s1.html/c/d#f,            /a/b, s1,    html, /c/d",
+            "/a/b.s1.s2.html/c/d#f,         /a/b, s1.s2, html, /c/d",
+            "/a/b.html/c/d.s.txt#f,         /a/b, null,  html, /c/d.s.txt",
+            "/a/b.s1.html/c/d.s.txt#f,      /a/b, s1,    html, /c/d.s.txt",
+            "/a/b.s1.s2.html/c/d.s.txt#f,   /a/b, s1.s2, html, /c/d.s.txt"
+    })
+    public void testExtractingValuesFromUri(String uri, String path, String selectorString, String extension,
+            String suffix) {
         final UriInfo uriInfo = new UriInfo(uri);
         boolean result = StringUtils.equals(uriInfo.getPath(), path);
         result &= StringUtils.equals(uriInfo.getSelectorString(), selectorString);
