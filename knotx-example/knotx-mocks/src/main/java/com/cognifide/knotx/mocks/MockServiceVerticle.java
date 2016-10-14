d9b41ef5271f64596bab2f8e2f32154a6c8d2d17
Marcin Czeczko <marcin.czeczko@cognifide.com>
2016-10-14 19:43:55 +0200

    Refactored mock service handlers to be able to serve multiple types of file contents

--- knotx-example/knotx-mocks/src/main/java/com/cognifide/knotx/mocks/MockServiceVerticle.java
+++ knotx-example/knotx-mocks/src/main/java/com/cognifide/knotx/mocks/MockServiceVerticle.java
@@ -19,35 +19,41 @@ package com.cognifide.knotx.mocks;
 
 
 import com.cognifide.knotx.mocks.service.MockServiceHandler;
-import com.cognifide.knotx.mocks.service.PostServiceHandler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Context;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.rxjava.core.AbstractVerticle;
-import io.vertx.rxjava.core.http.HttpServer;
-import io.vertx.rxjava.ext.web.Router;
-import io.vertx.rxjava.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.ErrorHandler;
+import rx.functions.Action2;
 
 public class MockServiceVerticle extends AbstractVerticle {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MockServiceVerticle.class);
 
-  private PostServiceHandler mockPostServiceHandler;
-  private MockServiceHandler mockServiceHandler;
-
   private HttpServer httpServer;
+
+  private Action2<RoutingContext, String> bouncer = (context, mockData) -> {
+    JsonObject responseBody = new JsonObject(mockData);
+    MultiMap formParams = context.request().params();
+    formParams.names().forEach(name -> responseBody.put(name, formParams.get(name)));
+    context.response().setStatusCode(200).end(responseBody.encodePrettily());
+  };
 
   @Override
   public void init(Vertx vertx, Context context) {
     super.init(vertx, context);
-    mockServiceHandler = new MockServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
-    mockPostServiceHandler = new PostServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
   }
 
   @Override
@@ -57,8 +63,10 @@ public class MockServiceVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
-    router.route().method(HttpMethod.POST).handler(mockPostServiceHandler);
-    router.route().method(HttpMethod.GET).handler(mockServiceHandler);
+    router.route().method(HttpMethod.POST).handler(createPostHandler());
+    router.route().method(HttpMethod.GET).handler(createGetHandler());
+
+    router.route().failureHandler(ErrorHandler.create(true));
 
     httpServer.requestHandler(router::accept).listen(config().getInteger("http.port"));
   }
@@ -67,4 +75,16 @@ public class MockServiceVerticle extends AbstractVerticle {
   public void stop() throws Exception {
     httpServer.close();
   }
+
+  private MockServiceHandler createGetHandler() {
+    return new MockServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
+  }
+
+  private MockServiceHandler createPostHandler() {
+    MockServiceHandler mockServiceHandler = new MockServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
+    return config().getBoolean("bouncing", false)
+        ? mockServiceHandler.withBodyProcessor(bouncer)
+        : mockServiceHandler;
+  }
+
 }
