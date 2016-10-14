bd59690629b5527db75a6620e654b843197f59ca
Marcin Czeczko <marcin.czeczko@cognifide.com>
2016-10-14 16:15:26 +0200

    Vertx way file reading in Mock service, routing per method type

--- knotx-example/knotx-mocks/src/main/java/com/cognifide/knotx/mocks/MockServiceVerticle.java
+++ knotx-example/knotx-mocks/src/main/java/com/cognifide/knotx/mocks/MockServiceVerticle.java
@@ -19,21 +19,26 @@ package com.cognifide.knotx.mocks;
 
 
 import com.cognifide.knotx.mocks.service.MockServiceHandler;
+import com.cognifide.knotx.mocks.service.PostServiceHandler;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 import io.vertx.core.Context;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.ext.web.Router;
+import io.vertx.rxjava.ext.web.handler.BodyHandler;
 
 public class MockServiceVerticle extends AbstractVerticle {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MockServiceVerticle.class);
 
+  private PostServiceHandler mockPostServiceHandler;
   private MockServiceHandler mockServiceHandler;
 
   private HttpServer httpServer;
@@ -41,15 +46,21 @@ public class MockServiceVerticle extends AbstractVerticle {
   @Override
   public void init(Vertx vertx, Context context) {
     super.init(vertx, context);
-    mockServiceHandler = new MockServiceHandler(config().getString("mock.data.root"));
+    mockServiceHandler = new MockServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
+    mockPostServiceHandler = new PostServiceHandler(config().getString("mock.data.root"), vertx.fileSystem());
   }
 
   @Override
   public void start() throws IOException, URISyntaxException {
     LOGGER.info("Starting <{}>", this.getClass().getSimpleName());
     httpServer = vertx.createHttpServer();
-    httpServer.requestHandler(mockServiceHandler)
-        .listen(config().getInteger("http.port"));
+
+    Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
+    router.route().method(HttpMethod.POST).handler(mockPostServiceHandler);
+    router.route().method(HttpMethod.GET).handler(mockServiceHandler);
+
+    httpServer.requestHandler(router::accept).listen(config().getInteger("http.port"));
   }
 
   @Override
