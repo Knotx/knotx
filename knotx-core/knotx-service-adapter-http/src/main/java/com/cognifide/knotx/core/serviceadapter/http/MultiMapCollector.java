5e87f7b9bdf5bd4360283485e3dd0eeb339a8f62
Marcin Czeczko <marcin.czeczko@cognifide.com>
2016-10-17 14:30:20 +0200

    Fixed packages of multimap collector utility

--- knotx-core/knotx-service-adapter-http/src/main/java/com/cognifide/knotx/core/serviceadapter/http/MultiMapCollector.java
+++ knotx-core/knotx-service-adapter-http/src/main/java/com/cognifide/knotx/core/serviceadapter/http/MultiMapCollector.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cognifide.knotx.engine;
+package com.cognifide.knotx.core.serviceadapter.http;
 
 import com.google.common.collect.ImmutableSet;
 
