fd60e4e981e2d44b1c185c05b55753e60a83f27e
Tomasz Michalak <tomasz.michalak@cognifide.com>
2016-10-20 20:58:19 +0200

    Base action knot maven module.

---
+++ knotx-core/knotx-knot-action/src/main/java/com/cognifide/knotx/knot/action/ActionKnotVerticle.java
@@ -0,0 +1,51 @@
+/*
+ * Knot.x - Reactive microservice assembler - Action Knot Verticle
+ *
+ * Copyright (C) 2016 Cognifide Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cognifide.knotx.knot.action;
+
+import io.vertx.core.Context;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.eventbus.Message;
+
+public class ActionKnotVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ActionKnotVerticle.class);
+
+  private ActionKnotConfiguration configuration;
+
+  @Override
+  public void init(Vertx vertx, Context context) {
+    super.init(vertx, context);
+    configuration = new ActionKnotConfiguration(config());
+  }
+
+  @Override
+  public void start() throws Exception {
+    LOGGER.debug("Starting <{}>", this.getClass().getName());
+
+  }
+
+  private void traceMessage(Message<JsonObject> message) {
+    if (LOGGER.isTraceEnabled()) {
+      LOGGER.trace("Got message from <{}> with value <{}>", message.replyAddress(), message.body().encodePrettily());
+    }
+  }
+}
