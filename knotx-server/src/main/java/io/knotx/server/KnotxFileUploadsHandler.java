a8d1036e95e77e0a99eceac16814b8fdb36bfa51
Maciej Laskowski <skejven@gmail.com>
2017-12-11 19:12:43 +0100

    File upload in examples

--- knotx-server/src/main/java/io/knotx/server/KnotxFileUploadsHandler.java
+++ knotx-server/src/main/java/io/knotx/server/KnotxFileUploadsHandler.java
@@ -19,17 +19,25 @@ import io.knotx.dataobjects.FileData;
 import io.knotx.dataobjects.KnotContext;
 import io.vertx.core.Handler;
 import io.vertx.reactivex.ext.web.RoutingContext;
+import java.util.Set;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KnotxFileUploadsHandler implements Handler<RoutingContext> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(KnotxFileUploadsHandler.class);
 
   @Override
   public void handle(RoutingContext context) {
     KnotContext knotContext = context.get(KnotContext.KEY);
 
     if (!context.fileUploads().isEmpty()) {
-      knotContext.setFilesData(
-          context.fileUploads().stream().map(FileData::new).collect(Collectors.toSet()));
+      final Set<FileData> uploadedFiles = context.fileUploads().stream()
+          .map(FileData::new)
+          .collect(Collectors.toSet());
+      LOGGER.debug("{} file(s) uploaded, updating KnotContext", uploadedFiles.size());
+      knotContext.setFilesData(uploadedFiles);
 
       context.put(KnotContext.KEY, knotContext);
     }
