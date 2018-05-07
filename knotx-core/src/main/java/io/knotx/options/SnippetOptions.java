71fa504c81a8117435028ea84b4b3d004e2c4afe
Maciej Laskowski <skejven@gmail.com>
2018-05-07 10:50:23 +0200

    Snippet options

---
+++ knotx-core/src/main/java/io/knotx/options/SnippetOptions.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2016 Cognifide Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.knotx.options;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Describes SnippetOptions Knot configuration
+ */
+@DataObject(generateConverter = true, publicConverter = false)
+public class SnippetOptions {
+
+  private String tagName;
+  private String paramsPrefix;
+
+  /**
+   * Default constructor
+   */
+  public SnippetOptions() {
+  }
+
+  /**
+   * Copy constructor
+   *
+   * @param other the instance to copy
+   */
+  public SnippetOptions(SnippetOptions other) {
+    this.tagName = other.tagName;
+    this.paramsPrefix = other.paramsPrefix;
+  }
+
+  /**
+   * Create an settings from JSON
+   *
+   * @param json the JSON
+   */
+  public SnippetOptions(JsonObject json) {
+    SnippetOptionsConverter.fromJson(json, this);
+  }
+
+  /**
+   * Convert to JSON
+   *
+   * @return the JSON
+   */
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    SnippetOptionsConverter.toJson(this, json);
+    return json;
+  }
+
+  /**
+   * @return a snippet tag name.
+   */
+  public String getTagName() {
+    return tagName;
+  }
+
+  /**
+   * Sets a Knot.x snippet HTML tag name. Default is 'script'
+   *
+   * @param tagName tag name
+   * @return a reference to this, so the API can be used fluently
+   */
+  public SnippetOptions setTagName(String tagName) {
+    this.tagName = tagName;
+    return this;
+  }
+
+  /**
+   * @return a snippet params prefix
+   */
+  public String getParamsPrefix() {
+    return paramsPrefix;
+  }
+
+  /**
+   * Sets Knot.x snippet parameters prefix. Default is 'data-knotx-'
+   * @param paramsPrefix prefix
+   * @return a reference to this, so the API can be used fluently
+   */
+  public SnippetOptions setParamsPrefix(String paramsPrefix) {
+    this.paramsPrefix = paramsPrefix;
+    return this;
+  }
+}
