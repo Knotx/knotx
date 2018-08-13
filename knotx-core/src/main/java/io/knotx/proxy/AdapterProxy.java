6c7a3e08378c604d7354b80619f564982146e340
tomasz.michalak <tomek.michalak@gmail.com>
2018-08-13 12:33:15 +0200

    Mark all Service Knot related classes deprecated: removing javadoc from proxy-gen interface.

--- knotx-core/src/main/java/io/knotx/proxy/AdapterProxy.java
+++ knotx-core/src/main/java/io/knotx/proxy/AdapterProxy.java
@@ -24,10 +24,6 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.DeliveryOptions;
 
-/**
- * @deprecated  As of release 1.3.1, replaced by <a href="https://github.com/Knotx/knotx-data-bridge/blob/master/api/src/main/java/io/knotx/databridge/api/DataSourceAdapterProxy.java">DataSourceAdapterProxy</a>
- * @see <a href="https://github.com/Knotx/knotx-data-bridge">Knot.x Data Bridge</a>
- */
 @Deprecated
 @ProxyGen
 @VertxGen
